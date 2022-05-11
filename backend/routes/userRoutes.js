e280884ccf09bb754ad9e1ef523ac45e97e14671
Kalyani Sahu <sahu.kalyani879@gmail.com>
2022-05-11 12:36:01 +0530

    jwt authorization done

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express=require('express');
 const router = express.Router();
-const {registerUSer , loginUSer , getMe } = require('../controllers/user.controller')
+const {registerUSer , loginUSer , getMe } = require('../controllers/user.controller');
+const { protect } = require('../middleware/authMiddleware');
 
 router.post('/' , registerUSer)
 router.post('/login' , loginUSer)
-router.get('/me' , getMe)
+router.get('/me',protect, getMe)
 
 
 module.exports = router;
