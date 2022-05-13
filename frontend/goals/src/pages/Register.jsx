2856cd4015af0ef40c6198b9aae1ab601317d9e6
Kalyani Sahu <sahu.kalyani879@gmail.com>
2022-05-13 07:38:25 +0530

    authSlice in redux done

--- frontend/goals/src/pages/Register.jsx
+++ frontend/goals/src/pages/Register.jsx
@@ -1,36 +1,79 @@
 import { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
 import { FaUser } from 'react-icons/fa'
+import { register, reset } from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
 
-const Register = () => {
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        password: '',
-        password2: '',
-      })
+function Register() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+  })
 
-      const { name, email, password, password2 } = formData
+  const { name, email, password, password2 } = formData
 
-      const onSubmit = (e)=>{
-        e.preventDefault()
-      }
-    const onChange = (e)=>{
-        setFormData((prevState) => ({
-            ...prevState,
-            [e.target.name]: e.target.value,
-          })) 
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  )
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message)
     }
+
+    if (isSuccess || user) {
+      navigate('/')
+    }
+
+    dispatch(reset())
+  }, [user, isError, isSuccess, message, navigate, dispatch])
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    if (password !== password2) {
+      toast.error('Passwords do not match')
+    } else {
+      const userData = {
+        name,
+        email,
+        password,
+      }
+
+      dispatch(register(userData))
+    }
+  }
+
+  if (isLoading) {
+    return <Spinner />
+  }
+
   return (
     <>
-    <section className='heading'>
+      <section className='heading'>
         <h1>
-            <FaUser /> Register
+          <FaUser /> Register
         </h1>
         <p>Please create an account</p>
-    </section>
-    <section className='form'>
-    <form onSubmit={onSubmit}>
-    <div className='form-group'>
+      </section>
+
+      <section className='form'>
+        <form onSubmit={onSubmit}>
+          <div className='form-group'>
             <input
               type='text'
               className='form-control'
@@ -74,13 +117,13 @@ const Register = () => {
               onChange={onChange}
             />
           </div>
-    </form> 
-    <div className='form-group'>
+          <div className='form-group'>
             <button type='submit' className='btn btn-block'>
               Submit
             </button>
           </div>
-    </section>
+        </form>
+      </section>
     </>
   )
 }
